2d6855974f614d318a2750ad2420ef9683d1ae77
samirmnd <samir_aec2007@yahoo.com>
2015-09-25 01:41:12 -0700

    added shapes.cpp

---
+++ shapes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "shapes.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+shape::shape() {
+
+}
+
+rectangle::rectangle() {
+
+}
+
+ellipse::ellipse() {
+
+}
+
+square::square() : rectangle(){
+	
+
+	}
+
+circle::circle() : ellipse(){
+
+}
