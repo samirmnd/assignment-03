89f19e01083bdbe3af143afa061fc5f8ad0e1b5d
samirmnd <samir_aec2007@yahoo.com>
2015-09-24 18:43:07 -0700

    added point.h

---
+++ point.h
@@ -0,0 +1,8 @@
+#ifndef POINT_H
+#define POINT_H
+// ----------------------------------------------------------------------------
+
+// ... everything besides the copyright information and file description
+
+// ----------------------------------------------------------------------------
+#endif  // POINT_H
